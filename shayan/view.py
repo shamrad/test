5aa34cededaf6173071929859c22978833af2ab7
mehrad abedi <abedi.mehrad@yahoo.com>
2016-12-09 18:37:12 +0330

    sign up page front

--- shayan/view.py
+++ shayan/view.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
+
+
 def Home(request):
-    return HttpResponse('<h1>this is home page</h1>')
+    if request.user.is_authenticated():
+        return render(request, 'home/home.html')
