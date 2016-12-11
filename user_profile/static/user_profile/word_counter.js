462f4e59738cc9fd5bdee315fd3972ba5bf92a5f
mehrad abedi <abedi.mehrad@yahoo.com>
2016-12-11 18:50:10 +0330

    add word counter

---
+++ user_profile/static/user_profile/word_counter.js
@@ -0,0 +1,26 @@
+counter = function() {
+    var value = $('#id_text').val();
+
+    if (value.length == 0) {
+        $('#wordCount').html(0);
+
+        return;
+    }
+
+    var regex = /\s+/gi;
+    var wordCount = value.trim().replace(regex, ' ').split(' ').length;
+
+
+    $('#wordCount').html(wordCount);
+console.log(wordCount)
+};
+
+$(document).ready(function() {
+    $('#id_text').click(counter);
+    $('#id_text').change(counter);
+    $('#id_text').keydown(counter);
+    $('#id_text').keypress(counter);
+    $('#id_text').keyup(counter);
+    $('#id_text').blur(counter);
+    $('#id-text').focus(counter);
+});
