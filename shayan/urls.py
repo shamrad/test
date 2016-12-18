9d7e9c8b0843ada2eff9cab82ae5c08c896bdabe
mehrad abedi <abedi.mehrad@yahoo.com>
2016-12-18 20:29:02 +0330

    adding template for reset password pages

--- shayan/urls.py
+++ shayan/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     url(r'^about-us/', aboutus, name='aboutus'),
 
     url(r'password/$', password_reset,{'template_name': 'user_profile/password_reset.html'}, name='reset_password'),
-    url(r'password/done/$', password_reset_done, name='password_reset_done'),
+    url(r'password/done/$', password_reset_done,{'template_name': 'user_profile/password_reset_done.html'}, name='password_reset_done'),
     url(r'password/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, name='password_reset_confirm'),
-    url(r'password/complete/$', password_reset_complete, name='password_reset_complete'),
+    url(r'password/complete/$', password_reset_complete,{'template_name': 'user_profile/password_reset_complete.html'}, name='password_reset_complete'),
 
-]
+ ]
