5b1c050208944bd1d3fb01811561da964a3fa55c
mehrad abedi <abedi.mehrad@yahoo.com>
2017-01-01 16:42:48 +0330

    teacher page backend

--- corrector/views.py
+++ corrector/views.py
@@ -8,15 +8,21 @@ from user_profile.models import Writing
 
 def Teacherindex(request):
     current_user = request.user
-    writing=Writing.objects.filter(score='0')
+    second = Writing.objects.exclude(corrector='').values('author').distinct()
+    free = Writing.objects.exclude(author=request.user).filter(score='0').exclude(author__in=second)
+
+    follow = Writing.objects.filter(corrector=request.user.username).values('author').distinct()
+    writing = Writing.objects.exclude(author=request.user).filter(author__in=follow, score='0')
+
     if current_user.teacher:
         context={
             'user': current_user,
+            'free': free,
             'writing': writing
         }
         return render(request, 'user_profile/teacherindex.html', context)
 
-    return redirect('user_profile:index')
+    return redirect('user_profile:index') #agar user teacher nabod redirect beshe
 
 class Score(LoginRequiredMixin,UpdateView):
     login_url = 'user_profile:login'
