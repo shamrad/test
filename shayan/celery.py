921b921a06e3f117cd95e0c54dfc68b6d58ed796
scorize <scorize@srv36174.screweb.com>
2017-01-24 00:19:12 +0100

    Celery

---
+++ shayan/celery.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import celery
+import os
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shayan.settings')
+app = celery.Celery('scorize', broker=settings.BROKER_URL)
+print(settings.BROKER_URL)
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
