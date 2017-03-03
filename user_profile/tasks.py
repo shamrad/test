628817cb3e160c5fafbc7a8bf3d998b95d593f51
zamani.shayan <zamani.shayan@yahoo.com>
2017-03-03 14:17:49 +0330

    final changes - celery2

--- user_profile/tasks.py
+++ user_profile/tasks.py
@@ -22,8 +22,8 @@ def ersal():
                                      'number': pending_lesson.order,
                                      'content': pending_lesson.content,
                                      'site': settings.SITE_URL,
-                                     'music_file': 'dars ha/'+ pending_lesson.whichcourse +'/Lesson ' + str(pending_lesson.order) + '.mp3',
-                                     'text_file': 'dars ha/'+ pending_lesson.whichcourse +'/' + str(pending_lesson.order) + '.pdf'})
+                                     'music_file': 'dars ha/'+ pending_lesson.whichcourse.name +'/Lesson ' + str(pending_lesson.order) + '.mp3',
+                                     'text_file': 'dars ha/'+ pending_lesson.whichcourse.name +'/' + str(pending_lesson.order) + '.pdf'})
         send_mail('دانلود درس شماره %s' % pending_lesson.order, pending_lesson.content, settings.DEFAULT_FROM_EMAIL,
                   [i.participant.email], fail_silently=False, html_message=msg_html)
         i.last_email_received += 1
@@ -38,6 +38,6 @@ def notif():
     free_writings = Writing.objects.filter(corrector=None)
     teachers = User.objects.filter(teacher=True)
     for x in teachers:
-        send_mail('New Writings Notification', 'Dear Professor, %s new free writings are available in your profile.'
-                                               ' Scorize Team' % free_writings.count(),
+        send_mail('New Writings Notification',
+                  'Dear Professor, %s new free writings are available in your profile. Scorize Team' % free_writings.count(),
                   settings.DEFAULT_FROM_EMAIL, [x.email], fail_silently=False)
