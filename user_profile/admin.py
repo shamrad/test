74c1a7ffc946c899685da352aed6553edc9403e6
mehrad abedi <abedi.mehrad@yahoo.com>
2017-05-08 17:32:55 +0430

    user admin

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import Writing,User, Subject, Teacherate, Price, Buy, Course, Lesson, Registration, Hamayesh, Event
+
+class MemberAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'date_joined', 'last_login', 'is_active')
+    list_display_links = ('email', 'first_name', 'last_name')
+    ordering = ('date_joined', 'last_login', 'email')
+    search_fields = ('email', 'first_name', 'last_name')
 
 admin.site.register(Writing)
 admin.site.register(User)
